69dd01318fa1ca183e03ee0de908e24122787029
iampeterjohn <peterjeche442@gmail.com>
2024-12-20 18:34:09 +0100

    project setup

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <h1>{{ hello }}</h1>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from "vue";
+
+export default defineComponent({
+  components: {},
+  setup() {
+    const hello = ref<string>("hello world");
+
+    return { hello };
+  },
+});
+</script>
+
+<style></style>
